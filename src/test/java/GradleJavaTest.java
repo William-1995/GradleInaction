3142542dc04290eca9a27acffb8d1ba40a19860f
William-1995 <2386604238@qq.com>
2023-06-25 21:54:20 +0800

    Create a gradle project

---
+++ src/test/java/GradleJavaTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class GradleJavaTest {
+
+    @Test
+    void testPrint() {
+        System.out.println("hello test.");
+    }
+}
---
+++ sub-projects/sub-project2/src/test/java/GradleJavaTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class GradleJavaTest {
+
+    @Test
+    void testPrint() {
+        System.out.println("hello gradle java unit test in child project.");
+    }
+}
