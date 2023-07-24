b13b9e0b6d8a578c0f81a794dca80b6ecd4ef7de
William-1995 <2386604238@qq.com>
2023-07-24 22:28:03 +0800

    Add springboot test by java and groovy

--- src/main/groovy/com/learning/Main.groovy
+++ src/main/groovy/com/learning/Main.groovy
@@ -1,5 +1,17 @@
 package com.learning
 
+import com.gradle.demo.SpringBootApplication
+import org.springframework.boot.SpringApplication
+
+
 static void main(String[] args) {
     println "Hello world!"
+}
+
+class GroovySpringBootApplication {
+
+    static void main(String[] args) {
+        println "Hello world!"
+        SpringApplication.run(SpringBootApplication.class, args);
+    }
 }
